16798bf4cb77ddb5b4a9d961b4d61a88617b9ea4
Shreeansh Srivastava <78229063+shreeansh2m2000@users.noreply.github.com>
2021-05-02 00:08:38 +0530

    Delete src directory

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const request = axios.create({
-   baseURL: 'https://youtube.googleapis.com/youtube/v3/',
-   params: {
-      key: 'AIzaSyBzFtSZSY08i4jEg_AyqmL_A3ManR5BGQ0',
-   },
-})
-
-export default request
